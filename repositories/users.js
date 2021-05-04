e0e5f1354b0a5f8a1c37fb605e5b4d9c260663d8
bgordon8 <gordonbrian16@gmail.com>
2021-05-04 15:03:50 -0600

    Adds getAll method to Users class

--- repositories/users.js
+++ repositories/users.js
@@ -13,6 +13,18 @@ class UsersRepository {
       fs.writeFileSync(this.filename, "[]");
     }
   }
+  async getAll() {
+    return JSON.parse(
+      await fs.promises.readFile(this.filename, {
+        encoding: "utf8",
+      })
+    );
+  }
 }
+const test = async () => {
+  const repo = new UsersRepository("users.json");
+  const users = await repo.getAll();
 
-new UsersRepository("users.json");
+  console.log(users);
+};
+test();
