b167f5397fb8991d3bcf27fce5f8e0d06fe20a07
bgordon8 <gordonbrian16@gmail.com>
2021-08-30 19:39:42 -0600

    Adds Parent repository, updates users repo

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cookieSession = require("cookie-session");
 const authRouter = require("./routes/admin/auth");
-
+const productsRouter = require("./routes/admin/products");
 const app = express();
 
 app.use(express.static("public"));
@@ -12,7 +12,9 @@ app.use(
     keys: ["lkjlkjoijsdgnw922t"],
   })
 );
+
 app.use(authRouter);
+app.use(productsRouter);
 
 app.listen(3000, () => {
   console.log("Listening on port 3000...");
