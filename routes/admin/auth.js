86254326024bae9cdaae9b72d302fd507202a6f2
bgordon8 <gordonbrian16@gmail.com>
2021-09-28 14:52:32 -0600

    Updates auth.js middleware

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { check, validationResult } = require("express-validator");
+const handleErrors = require("./middleware");
 
 const usersRepo = require("../../repositories/users");
 const signupTemplate = require("../../views/admin/auth/signup");
@@ -25,13 +25,9 @@ router.get("/signin", (req, res) => {
 router.post(
   "/signup",
   [requireEmail, requirePassword, requirePasswordConfirmation],
+  handleErrors(signupTemplate),
   async (req, res) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      return res.send(signupTemplate({ req, errors }));
-    }
-    const { email, password, passwordConfirmation } = req.body;
+    const { email, password } = req.body;
     const user = await usersRepo.create({ email, password });
     req.session.userId = user.id;
     res.send("Account created!!");
@@ -46,11 +42,8 @@ router.get("/signout", (req, res) => {
 router.post(
   "/signin",
   [requireValidEmail, requireValidPasswordUser],
+  handleErrors(signinTemplate),
   async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.send(signinTemplate({ errors }));
-    }
     const { email } = req.body;
     const user = await usersRepo.getOneBy({ email });
     req.session.userId = user.id;
