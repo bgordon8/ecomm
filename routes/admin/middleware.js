beee9c6f392eb76a93c89f8b29d7065d37f2b123
bgordon8 <gordonbrian16@gmail.com>
2021-09-27 19:13:56 -0600

    Update errors handler

--- routes/admin/middleware.js
+++ routes/admin/middleware.js
@@ -2,6 +2,12 @@ const { validationResult } = require("express-validator");
 
 module.exports = {
   handleErrors(templateFunc) {
-    return (req, res, next) => {};
+    return (req, res, next) => {
+      const errors = validationResult(req);
+
+      if (!errors.isEmpty()) {
+        return res.send(templateFunc({ errors }));
+      }
+    };
   },
 };
