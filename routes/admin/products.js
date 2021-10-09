fd8318f0e0127d9896b20267775d671f3e5c89ac
bgordon8 <gordonbrian16@gmail.com>
2021-10-09 12:35:20 -0600

    Updates product routes

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -3,6 +3,8 @@ const multer = require("multer");
 
 const { handleErrors, requireAuth } = require("./middleware");
 const productsRepo = require("../../repositories/products");
+
+const productsEditTemplate = require('../../views/admin/products/edit')
 const productsNewTemplate = require("../../views/admin/products/new");
 const productsIndexTemplate = require("../../views/admin/products/index");
 const { requireTitle, requirePrice } = require("./validators");
@@ -36,6 +38,11 @@ router.post(
 
 router.get("/admin/products/:id/edit", (req, res) => {
   const product = await productsRepo.getOne(req.params.id)
+
+  if (!product) {
+    return res.send("Product not found")
+  }
+  res.send(productsEditTemplate({product}))
 });
 
 module.exports = router;
