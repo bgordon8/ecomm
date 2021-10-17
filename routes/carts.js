92551dc8140034d864ae4ad82b8742b16546afaf
bgordon8 <gordonbrian16@gmail.com>
2021-10-16 19:48:47 -0600

    Updates cart post route and adds product.id to cart

--- routes/carts.js
+++ routes/carts.js
@@ -1,5 +1,20 @@
 const express = require("express");
-
+const cartsRepo = require("../repositories/carts");
 const router = express.Router();
+
+router.post("/cart/products", async (req, res) => {
+  console.log(req.body.productId);
+  let cart;
+  if (!req.session.cartId) {
+    cart = await cartsRepo.create({ items: [] });
+    req.session.cartId = cart.id;
+  } else {
+    cart = await cartsRepo.getOne(req.session.cartId);
+  }
+  console.log(cart);
+  res.send("product added");
+});
+
+router.get("/", (req, res) => {});
 
 module.exports = router;
