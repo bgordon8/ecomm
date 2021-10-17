631c5f4f211d0b2358ab74ec7ccb4b7b8089450a
bgordon8 <gordonbrian16@gmail.com>
2021-10-16 19:30:46 -0600

    Adds carts.js boiler plate routes/repository

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cookieSession = require("cookie-session");
 const authRouter = require("./routes/admin/auth");
 const productsRouter = require("./routes/admin/products");
 const productsUserRouter = require("./routes/products");
+const cartsRouter = require("./routes/carts");
 const app = express();
 
 app.use(express.static("public"));
@@ -17,6 +18,7 @@ app.use(
 app.use(authRouter);
 app.use(productsRouter);
 app.use(productsUserRouter);
+app.use(cartsRouter);
 
 app.listen(3000, () => {
   console.log("Listening on port 3000...");
