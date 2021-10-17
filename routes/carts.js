9252e1b04a62f5a8f2c6ce70c81da37246de4535
bgordon8 <gordonbrian16@gmail.com>
2021-10-16 20:04:35 -0600

    Updates cart post route, test add to cart

--- routes/carts.js
+++ routes/carts.js
@@ -11,6 +11,19 @@ router.post("/cart/products", async (req, res) => {
   } else {
     cart = await cartsRepo.getOne(req.session.cartId);
   }
+  const existingItem = cart.items.find(
+    (item) => item.id === req.body.productId
+  );
+
+  if (existingItem) {
+    existingItem.quantity++;
+  } else {
+    cart.items.push({ id: req.body.productId, quantity: 1 });
+  }
+  await cartsRepo.update(cart.id, {
+    items: cart.items,
+  });
+
   console.log(cart);
   res.send("product added");
 });
